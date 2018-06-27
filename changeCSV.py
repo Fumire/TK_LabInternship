c84ef6e68396ed1d6740a85874d55b5629d02a80
Jaewoong Lee <230@fumire.moe>
2018-06-27 13:43:10 +0900

    change txt files to csv

---
+++ changeCSV.py
@@ -0,0 +1,21 @@
+import sys
+
+for name in sys.argv[1:]:
+    txtFileName = name.strip()
+    csvFileName = txtFileName[:txtFileName.rfind(".")] + ".csv"
+
+    txtFile = open(txtFileName, "r")
+    csvFile = open(csvFileName, "w")
+
+    while True:
+        line = txtFile.readline()
+        if not line: break
+        elif line[0] == "#":
+            #csvFile.write(line)
+            continue;
+        else:
+            line = line.replace("\t", ",")
+            csvFile.write(line)
+
+    txtFile.close()
+    csvFile.close()
