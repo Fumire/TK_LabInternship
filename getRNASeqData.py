a293a1b1b52f80ed8ac57e71f664be7aab62b88a
Jaewoong Lee <230@fumire.moe>
2018-06-28 10:51:08 +0900

    construct all basic functions in get RNA-Seq

--- getRNASeqData.py
+++ getRNASeqData.py
@@ -6,7 +6,7 @@ def RNASeqwithGene(fileName, review=True):
 
     RNASeqFile = open(fileName, "r")
     
-    for _ in range(2): RNASeqFile.readline()
+    for _ in range(2): assert RNASeqFile.readline()
 
     for organ in RNASeqFile.readline().split("\t")[:]:
         assert organ not in geneList
@@ -26,7 +26,6 @@ def RNASeqwithGene(fileName, review=True):
                 continue
             if review and float(val_i) == 0: continue
             geneList[gene][organList[i]] = float(val_i)
-
     RNASeqFile.close()
 
     if review:
@@ -42,7 +41,7 @@ def RNASeqwithOrgan(fileName, review=True):
     organName = list()
     RNASeqFile = open(fileName, "r")
     
-    for _ in range(2): RNASeqFile.readline()
+    for _ in range(2): assert RNASeqFile.readline()
 
     for organ in RNASeqFile.readline().split("\t")[2:]:
         assert organ not in organList
@@ -58,7 +57,6 @@ def RNASeqwithOrgan(fileName, review=True):
             assert gene not in organList[organName[i]]
             if review and float(val_i) == 0: continue
             organList[organName[i]][gene] = float(val_i)
-
     RNASeqFile.close()
 
     if review:
@@ -67,14 +65,83 @@ def RNASeqwithOrgan(fileName, review=True):
 
     return organList
 
+def RNASeqOrganName(fileName):
+    organList = dict()
+    RNASeqFile = open(fileName, "r")
+    
+    for _ in range(2): assert RNASeqFile.readline()
+
+    for i, ch_i in enumerate(RNASeqFile.readline().split("\t")[2:]):
+        assert ch_i not in organList
+        organList[ch_i] = i
+    RNASeqFile.close()
+
+    return organList
+
+def RNASeqGeneName(fileName):
+    geneList = dict()
+    RNASeqFile = open(fileName, "r")
+
+    for _ in range(2): assert RNASeqFile.readline()
+
+    seq = 0
+    while True:
+        line = RNASeqFile.readline()
+        if not line: break
+        gene = line.split("\t")[0]
+        try: assert gene not in geneList
+        except:
+            print(gene, geneList[gene])
+            seq += 1
+        geneList[gene] = seq 
+        seq += 1
+    RNASeqFile.close()
+    
+    return geneList
+
+def selectedGene(fileName, geneNum):
+    geneList = dict()
+    organList = list()
+    RNASeqFile = open(fileName, "r")
+
+    for _ in range(2): assert RNASeqFile.readline()
+
+    for organ in RNASeqFile.readline().split("\t"):
+        organList.append(organ)
+
+    for _ in range(geneNum, 0, -1): assert RNASeqFile.readline()
+
+    for i, val_i in enumerate(RNASeqFile.readline().split("\t")):
+        if i<2: geneList[organList[i]] = val_i
+        else: geneList[organList[i]] = float(val_i)
+    RNASeqFile.close()
+    return geneList
+
+def selectedOrgan(fileName, organNum):
+    organList = dict()
+    RNASeqFile = open(fileName, "r")
+
+    for _ in range(2+1): assert RNASeqFile.readline()
+
+    while True:
+        line = RNASeqFile.readline()
+        if not line: break
+        line = line.split("\t")
+        organList[line[0]] = float(line[organNum+2])
+    RNASeqFile.close()
+    return organList
+
+def selectedGeneOrgan(fileName, geneNum, organNum):
+    with open(fileName, "r") as RNASeqFile:
+        for _ in range(2+1): assert RNASeqFile.readline()
+        for _ in range(geneNum, 0, -1): assert RNASeqFile.readline()
+        return float(RNASeqFile.readline().split("\t")[2+organNum])
+
 if __name__ == "__main__":
-    name = "./GTEx/RNA-Seq/GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_median_tpm.gct"
-    pool1 = ThreadPool(processes=1)
-    pool2 = ThreadPool(processes=2)
-
-    asyncResult1 = pool1.apply_async(RNASeqwithGene, (name, False))
-    asyncResult2 = ThreadPool(processes=1).apply_async(RNASeqwithOrgan, (name, False))
-
-    withGene = asyncResult1.get()
-    withOrgan = asyncResult2.get()
-    print(len(withOrgan), len(withGene))
+    name = "./GTEx/RNA-Seq/GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_tpm.gct"
+    #name = "./GTEx/RNA-Seq/GTEx_Analysis_2016-01-15_v7_STARv2.4.2a_junctions.gct"
+    organList = RNASeqOrganName(name); print(len(organList));
+    geneList = RNASeqGeneName(name); print(len(geneList));
+    selGene = selectedGene(name, 1234); print(len(selGene));
+    selOrgan = selectedOrgan(name, 33); print(len(selOrgan));
+    selGeneOrgan = selectedGeneOrgan(name, 1234, 33); print(selGeneOrgan);
