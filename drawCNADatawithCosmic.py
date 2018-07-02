725dac55720dd9ab0bf58d3a398734329219357f
Jaewoong Lee <230@fumire.moe>
2018-07-02 12:51:13 +0900

    change figure size; track execution

--- drawCNADatawithCosmic.py
+++ drawCNADatawithCosmic.py
@@ -12,21 +12,27 @@ now = time.strftime("%m%d%H%M%S")
 seq = 0
 for gene, val in data.items():
     name = gene.split("+")[0]
+    #if val == 0: continue
     if (name, val) in ans: ans[(name, val)] += 1
     else: ans[(name, val)] = 1
-    if seq == 10000: break
-    else: seq += 1
+    #if seq == 10000: break
+    #else: seq += 1
 del data
+print("Load Data")
 
 x = list()
 y = list()
 color = list()
 
-for val in ans:
+gene = list(ans.keys())
+gene.sort()
+
+for val in gene:
     x.append(val[0])
     y.append(val[1])
     color.append(ans[val])
 del ans
+print("Load ans")
 
 x = np.array(x)
 y = np.array(y)
@@ -38,7 +44,9 @@ plt.colorbar()
 
 plt.xlabel("Gene")
 plt.ylabel("CNA")
+plt.xticks([])
 
 fig = plt.gcf()
+fig.set_size_inches(24, 18)
 plt.show()
 fig.savefig("COSMIC_"+now+".png")
