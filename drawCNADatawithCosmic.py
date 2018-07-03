9236d0db6a4f64e53d93b3630ffb9cc42dcddbeb
Jaewoong Lee <230@fumire.moe>
2018-07-03 10:08:54 +0900

    control size

--- drawCNADatawithCosmic.py
+++ drawCNADatawithCosmic.py
@@ -5,24 +5,23 @@ import numpy as np
 import getCNAData as cna
 import time
 
+now = time.strftime("%m%d%H%M%S")
 data = cna.onlyCNAFromCosmic("../COSMIC/CosmicCompleteCNA.tsv")
 ans = dict()
-now = time.strftime("%m%d%H%M%S")
 
-seq = 0
 for gene, val in data.items():
-    name = gene.split("+")[0]
+    name = gene.split("+")[1]
     #if val == 0: continue
+    if abs(val) < 100: continue
     if (name, val) in ans: ans[(name, val)] += 1
     else: ans[(name, val)] = 1
-    #if seq == 10000: break
-    #else: seq += 1
 del data
 print("Load Data")
 
 x = list()
 y = list()
 color = list()
+big = list()
 
 gene = list(ans.keys())
 gene.sort()
@@ -31,20 +30,25 @@ for val in gene:
     x.append(val[0])
     y.append(val[1])
     color.append(ans[val])
+    big.append(ans[val] * 100)
 del ans
 print("Load ans")
 
 x = np.array(x)
 y = np.array(y)
+color = np.array(color)
+big = np.array(big)
 
 plt.figure()
 
-plt.scatter(x, y, s=color, c=color, alpha=0.5)
+plt.scatter(x, y, s=big, c=color, alpha=0.5)
 plt.colorbar()
 
+plt.title("Log Scale")
 plt.xlabel("Gene")
 plt.ylabel("CNA")
 plt.xticks([])
+plt.yscale('log')
 
 fig = plt.gcf()
 fig.set_size_inches(24, 18)
