bfe3ec1656185e84600a8948777a643f05b12de1
Jaewoong Lee <230@fumire.moe>
2018-07-20 09:26:06 +0900

    add function getSegmentNondiploidCNA()

--- get1000Data.py
+++ get1000Data.py
@@ -5,6 +5,7 @@ def getFileNames(filename):
         line = nameFile.readline()
         if not line: break
         nameList.append(line.strip())
+    nameFile.close()
     return nameList
 
 def getSegmentDiploidHugo():
@@ -27,6 +28,26 @@ def getSegmentDiploidHugo():
     ans.sort()
     return ans
 
+def getSegmentNondiploidHugo():
+    ans = list()
+    openFileList = getFileNames("cnvSegmentsNondiploidBeta.txt")
+
+    for openFile in openFileList:
+        thisFile = open("../1000Gene/cnvSegmentsNondiploidBeta/" + openFile, "r")
+        while True:
+            line = thisFile.readline()
+            if not line: break
+            elif line[0] in ["#", "\n", ">"]: continue
+            line = line.split("\t")
+
+            if line[9] == '': continue
+            hugo = line[9].split(";")
+            for gene in hugo:
+                if gene not in ans: ans.append(gene)
+            thisFile.close()
+    ans.sort()
+    return ans
+
 def getSegmentDiploidCNA():
     ans = {"-":[], "=":[], "+":[]}
     openFileList = getFileNames("cnvSegmentsDiploidBeta.txt")
@@ -43,9 +64,32 @@ def getSegmentDiploidCNA():
         thisFile.close()
     return ans
 
+def getSegmentNondiploidCNA():
+    ans = {"-":[], "=":[], "+":[]}
+    openFileList = getFileNames("cnvSegmentsNondiploidBeta.txt")
+
+    for openFile in openFileList:
+        thisFile = open("../1000Gene/cnvSegmentsNondiploidBeta/" + openFile, "r")
+        while True:
+            line = thisFile.readline()
+            if not line: break
+            elif line[0] in ["#", "\n", ">"]: continue
+            line = line.split("\t")
+
+            if line[6] in ans: ans[line[6]].append(int(line[5]))
+        thisFile.close()
+    return ans
+
 if __name__ == "__main__":
-    #p = getSegmentDiploidHugo()
+    p = getSegmentDiploidHugo(); print(len(p), p[:10]);
+    p = getSegmentDiploidHugo(); print(len(p), p[:10]);
+
     p = getSegmentDiploidCNA()
-    #print(len(p["-"]), len(p["="]), len(p["+"]))
+    print(len(p["-"]), len(p["="]), len(p["+"]))
     a1, a2 = min(p["="]), max(p["="])
-    for i in range(a1, a2+1): print(p["="].count(i))
+    for i in range(a1, a2+1): print(i, p["="].count(i))
+
+    p = getSegmentNondiploidCNA()
+    print(len(p["-"]), len(p["="]), len(p["+"]))
+    a1, a2 = min(p["="]), max(p["="])
+    for i in range(a1, a2+1): print(i, ["="].count(i))
